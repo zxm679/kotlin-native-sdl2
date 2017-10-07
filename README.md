22ede77c4f4a96a282e760298478868bafea75a4
Bruce Davidson <darkoverlordofdata@gmail.com>
2017-10-07 00:17:01 -0700

    daily

--- README.md
+++ README.md
@@ -6,3 +6,4 @@ I like scala syntax slightly more than I do kotlin, however scala native isn't p
 
 Then there is the scala build tool - sbt. Konanc is slow, but it is much simpler to use than sbt. And no gradle either - I like that.
  
+Very comparable to scala native, but c interop is much more transparent - it just works
--- src/main/kotlin/shmupwarz.kt
+++ src/main/kotlin/shmupwarz.kt
@@ -1,19 +1,26 @@
 /*
- * Copyright 2010-2017 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
+ * MIT License
+ * 
+ * Copyright (c) 2017 Bruce Davidson
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+*/
 import kotlinx.cinterop.*
 import sdl.*
 
@@ -99,15 +106,15 @@ class Game(val width: Int, val height: Int) {
         rect.x = 0
         rect.y = 0
 
-        texture = loadImage(window, renderer, "images/background.bmp")
+        texture = loadImage(window, renderer, "images/background.png")
     }
 
     private fun loadImage(win: CPointer<SDL_Window>, ren: CPointer<SDL_Renderer>, imagePath: String): CPointer<SDL_Texture> {
-        val bmp = SDL_LoadBMP_RW(SDL_RWFromFile(imagePath, "rb"), 1);
+        val bmp = IMG_LoadPNG_RW(SDL_RWFromFile(imagePath, "rb"));
         if (bmp == null) {
             SDL_DestroyRenderer(ren)
             SDL_DestroyWindow(win)
-            println("SDL_LoadBMP_RW Error: ${get_SDL_Error()}")
+            println("IMG_LoadPNG_RW Error: ${get_SDL_Error()}")
             SDL_Quit()
             throw Error()
         }
